b31a4507b038e77edd8bf81d42a985ee05eec3aa
李卓原 <zhuoyuan93@gmail.com>
2018-12-13 10:56:36 +0800

    代码优化

--- lib/flutter_share_me.dart
+++ lib/flutter_share_me.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/services.dart';
 
 class FlutterShareMe {
-  static const MethodChannel _channel = const MethodChannel('flutter_share_me');
+  final MethodChannel _channel = const MethodChannel('flutter_share_me');
 
   Future<String> shareToFacebook({String url = '', String msg = ''}) async {
     final Map<String, Object> arguments = Map<String, dynamic>();
