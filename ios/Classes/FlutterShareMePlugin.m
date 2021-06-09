f0be2e4c70c90cf01b7af52bacbf65deed3f9125
Mohammad Norhamizan <mnorhamizan@me.com>
2021-06-10 02:33:43 +0800

    iOS Implementation for 0.92

--- ios/Classes/FlutterShareMePlugin.m
+++ ios/Classes/FlutterShareMePlugin.m
@@ -2,19 +2,99 @@
 
 @implementation FlutterShareMePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_share_me"
-            binaryMessenger:[registrar messenger]];
-  FlutterShareMePlugin* instance = [[FlutterShareMePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter_share_me"
+                                     binaryMessenger:[registrar messenger]];
+    FlutterShareMePlugin* instance = [[FlutterShareMePlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"shareFacebook" isEqualToString:call.method]) {
+        [self shareToFacebook: call.arguments[@"url"] message: call.arguments[@"msg"] result:result];
+    } else if ([@"shareTwitter" isEqualToString:call.method]) {
+        [self shareToTwitter: call.arguments[@"url"] message: call.arguments[@"msg"] result:result];
+    } else if ([@"shareWhatsApp" isEqualToString:call.method]) {
+        [self shareToWhatsApp:call.arguments[@"msg"] result:result];
+    } else if ([@"shareWeChat" isEqualToString:call.method]) {
+        [self shareToWeChat:call.arguments[@"msg"] result:result];
+    } else if ([@"system" isEqualToString:call.method]) {
+        [self shareViaSystem:call.arguments[@"msg"] result:result];
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
+- (void)shareToFacebook: (NSString *)url message: (NSString *) msg result:(FlutterResult)result {
+    FBSDKShareLinkContent *content = [[FBSDKShareLinkContent alloc] init];
+    content.contentURL = [NSURL URLWithString:url];
+    content.quote = msg;
+    UIViewController *vc = [UIApplication sharedApplication].delegate.window.rootViewController;
+    [FBSDKShareDialog showFromViewController:vc
+                                 withContent:content
+                                    delegate:nil];
+    result(nil);
+}
+
+- (void)shareToTwitter: (NSString *)url message: (NSString *) msg result:(FlutterResult)result {
+    msg = [msg stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    url = [url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    NSURL *nsUrl = [NSURL URLWithString:[NSString stringWithFormat:@"https://twitter.com/intent/tweet?text=%@&url=%@", msg ,url]];
+    if ([[UIApplication sharedApplication] canOpenURL:nsUrl]) {
+        [[UIApplication sharedApplication] openURL:nsUrl];
+    }
+    result(nil);
+}
+
+- (void)shareToWhatsApp: (NSString *) msg result:(FlutterResult)result {
+    msg = [msg stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    UIApplication *application = [UIApplication sharedApplication];
+    NSURL *whatsappURL = [NSURL URLWithString:[NSString stringWithFormat:@"whatsapp://send?text=%@", msg]];
+    
+    if ([application respondsToSelector:@selector(openURL:options:completionHandler:)]) {
+        [application openURL:whatsappURL options:@{}
+           completionHandler:^(BOOL success) {
+        }];
+    } else {
+        BOOL success = [application openURL:whatsappURL];
+        if (success) result(@"success");
+        result(@"error");
+    }
+}
+
+- (void)shareToWeChat: (NSString *) msg result:(FlutterResult)result {
+    NSArray *objectsToShare = @[msg];
+    
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
+    
+    NSArray *excludeActivities = @[UIActivityTypeAirDrop,
+                                   UIActivityTypeAddToReadingList,
+                                   UIActivityTypePostToFlickr,
+                                   UIActivityTypeCopyToPasteboard,
+                                   UIActivityTypeSaveToCameraRoll,
+                                   UIActivityTypeAssignToContact,
+                                   UIActivityTypeMessage,
+                                   UIActivityTypeMail,
+                                   UIActivityTypePostToTwitter];
+    
+    activityVC.excludedActivityTypes = excludeActivities;
+    
+    UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
+    [controller presentViewController:activityVC animated:YES completion:nil];
+    result(nil);
+}
+
+- (void)shareViaSystem: (NSString *) msg result:(FlutterResult)result {
+    UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
+    UIActivityViewController *activityViewController =
+    [[UIActivityViewController alloc] initWithActivityItems:@[ msg ]
+                                      applicationActivities:nil];
+    activityViewController.popoverPresentationController.sourceView = controller.view;
+    //    if (!CGRectIsEmpty(origin)) {
+    //        activityViewController.popoverPresentationController.sourceRect = origin;
+    //    }
+    [controller presentViewController:activityViewController animated:YES completion:nil];
+    result(nil);
 }
 
 @end
--- ios/flutter_share_me.podspec
+++ ios/flutter_share_me.podspec
@@ -3,7 +3,7 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'flutter_share_me'
-  s.version          = '0.0.1'
+  s.version          = '0.9.2'
   s.summary          = 'Flutter Plugin for sharing contents to social media.'
   s.description      = <<-DESC
 Flutter Plugin for sharing contents to social media.
@@ -15,6 +15,9 @@ Flutter Plugin for sharing contents to social media.
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
+
+  s.dependency 'FBSDKShareKit', '5.11.1'
+  s.dependency 'FBSDKCoreKit', '5.12.0'
 
   s.ios.deployment_target = '8.0'
 end
