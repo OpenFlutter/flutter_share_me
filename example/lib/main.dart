ff22be1ab5839538679bb8a6721d73d6a2981b60
Jigar Fumakiya <jigarfumakiya@users.noreply.github.com>
2021-08-31 10:00:27 +0530

    Removed my number.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -123,7 +123,7 @@ class _MyAppState extends State<MyApp> {
         break;
       case Share.whatsapp_personal:
         response = await flutterShareMe.shareWhatsAppPersonalMessage(
-            message: msg, phoneNumber: '+919574957435');
+            message: msg, phoneNumber: 'phone-number-with-country-cooe');
         break;
       case Share.share_instagram:
         response = await flutterShareMe.shareToInstagram(imagePath: file!.path);
